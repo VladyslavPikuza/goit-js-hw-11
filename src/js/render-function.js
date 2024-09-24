3870f16b4801f431a705da4a74b62b271610b4bc
Vladyslav <168086670+VladyslavPikuza@users.noreply.github.com>
2024-09-24 11:39:44 +0300

    3123

--- src/js/render-function.js
+++ src/js/render-function.js
@@ -10,10 +10,10 @@ export function renderGallery(images) {
           <img src="${webformatURL}" alt="${tags}" loading="lazy" />
         </a>
         <div class="info">
-          <p><b>Likes:</b> ${likes}</p>
-          <p><b>Views:</b> ${views}</p>
-          <p><b>Comments:</b> ${comments}</p>
-          <p><b>Downloads:</b> ${downloads}</p>
+          <p><b>Likes</b> ${likes}</p>
+          <p><b>Views</b> ${views}</p>
+          <p><b>Comments</b> ${comments}</p>
+          <p><b>Downloads</b> ${downloads}</p>
         </div>
       </div>`;
   }).join('');
